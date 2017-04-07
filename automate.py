302e94725d2ca006f4704149cb48a23db9a5c1fc
Ian O'Heir <ianoheir@email.arizona.edu>
2017-04-07 16:45:46 -0700

    Create automate.py

---
+++ automate.py
@@ -0,0 +1,26 @@
+answer=input("What category does the clients problem fall under? 1=Dormant 2=SkuMigration)
+             if (str(answer)='1'):
+             	answer=input("Which dormant? 1=EXO 2=ProPlus 3=SPO 4=SFB)
+                             if (str(answer)=='1'):
+                             	print("EXO help")
+                             elif (str(answer)=='2'):
+                             	print("ProPlus help")
+                             elif (str(answer)=='3'):
+                             	print("SPO help")
+                             elif (str(answer)=='4'):
+                             	print("SFB help")
+             elif (str(answer)=='2'):
+             	print("1. Go to the admin portal")
+                print("2. Click billing then subscriptions")
+                print("3. Click renew on the right hand side of the tap that swipes in")
+                print("4. if you want the target plan hit renew now (ex small business premium becoming business premium by default)  
+")
+                print("5. otherwise hit see other renewal options which will bring up all subscriptions options ")
+                print("6. click plan and go over information to make sure the users licenses and price are correct")
+                print("7. enter payment info ( blur screen for this process)")
+                print("8. follow 4 step process once payment info is completed (just need to keep hitting next should take less than one hour)")
+                print("9. users on accounts may need to sign in again or download new apps depending on how old the previous version was.")
+                
+                             	
+                             	
+    
